3c0d72d03ee9518a4a3afa0d4d00888ea288319e
Daniel Klitzner <daniel.klitzner@yahoo.com>
2020-11-18 14:41:16 +0100

    Finished Script for renaming Blender PBR Textures

--- PythonRenameShit.py
+++ PythonRenameShit.py
@@ -5,23 +5,46 @@
 # importing os module 
 import os
 import re
+
+# Hardcoded this Path because it probably doesnt exist on your System so you dont fuck shit up by executing it
 path = '/Users/Dani/Documents/PythonTest'
 files = os.listdir(path)
   
-# Function to rename multiple files 
-def main(): 
+# Main Function
+def main():
+    addUnderscores()
+
+
+# Function to add Underscores
+def addUnderscores(): 
   
     for count, filename in enumerate(os.listdir(path)): 
         src = path + '/' + filename
-        dst = path + '/' + re.sub(r"(\w)([A-Z])", r"\1 \2", filename)
-          
-        # rename() function will 
-        # rename all the files 
-        print('renaming: ' + src + ' to ' + dst)
+        dst = path + '/' + re.sub(r"(\w)([A-Z])", r"\1_\2", filename)
+        #dst2 --> removing double Underscores
+        dst2 = re.sub('_+', '_', dst)
+
+        #call renameColor Function with our Folder before we rename the Folder
+        renameColor(src)
+
+        #remanes and adds Underscores between Upercase Letters
         os.rename(src, dst) 
+
+        #remove double Underscores
+        os.rename(dst, dst2)
+        
+
+# Function for renaming .._basecolor.jpg to .._color.jpg
+def renameColor(colorPath):
+    for count, filename in enumerate(os.listdir(colorPath)):
+        src = colorPath + '/' + filename
+        dst = colorPath + '/' + re.sub('basecolor', 'color', filename)
+
+        os.rename(src, dst)
+
   
 # Driver Code 
 if __name__ == '__main__': 
       
-    # Calling main() function 
-    main() 
+    # Calling function s
+    main()
